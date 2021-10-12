75859848461163c3c95611e94664709d61823a24
Chris Yeung <chris.yeung@shearwatersystems.com>
2021-10-12 10:49:54 +0100

    added remote test

---
+++ remote-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -a
+source .env.local
+set +a
+
+# run separately:
+python -m jobrunner.sync
+python -m jobrunner.run
+
+#eval $(minikube docker-env)
+# docker build -t opensafely-job-runner:latest .
+# docker build -t cohortextractor:latest -f ../cohort-extractor/Dockerfile ../cohort-extractor/.
+# docker tag cohortextractor:latest ghcr.io/opensafely-core/cohortextractor:latest
+
+az acr build --registry ccbidevdsacr --image opensafely-job-runner:latest -f Dockerfile .
